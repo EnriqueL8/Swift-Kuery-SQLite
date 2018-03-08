60dc7c613cb085dc6540ce75e8df763d20e1b3e7
Enrique Lacal <Enrique.Lacal.Bereslawski@ibm.com>
2018-03-08 11:42:42 +0000

    fix: Update to Swift 4 and fix test

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:4.0
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -19,11 +20,26 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKuerySQLite",
-    targets: [Target(name: "SwiftKuerySQLite", dependencies: [.Target(name: "CSQLite")]),
-              Target(name: "CSQLite")
+    products: [
+      .library(
+        name: "SwiftKuerySQLite",
+        targets: ["SwiftKuerySQLite"]
+      )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0, minor: 13),
-        ],
-    exclude: ["Configuration", "Scripts"]
+        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "1.1.0"),
+    ],
+    targets: [
+      .target(
+        name: "SwiftKuerySQLite",
+        dependencies: ["SwiftKuery", "CSQLite"]
+      ),
+      .target(
+        name: "CSQLite"
+      ),
+      .testTarget(
+        name: "SwiftKuerySQLiteTests",
+        dependencies: ["SwiftKuerySQLite"]
+      )
+    ]
 )
